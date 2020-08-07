c2e5b8d7099a270191e8d8fdbb9a3c814eadb4be
SAMAYV <samay@iitg.ac.in>
2020-08-08 02:24:34 +0530

    user profile created

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 
 // show register form
 router.get("/register", function(req, res){
@@ -14,7 +15,13 @@ router.get("/login", function(req, res){
 });
 
 router.post("/register",function(req,res){
-	var newUser = new User({username:req.body.username});
+	var newUser = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar
+	});
 	// eval(require('locus'));
 	if(req.body.adminCode === 'secretcode123'){
 		newUser.isAdmin = true;
@@ -47,6 +54,23 @@ router.get("/logout",function(req,res){
 	req.logout();
 	req.flash("success","Logged you out");
 	res.redirect("/campgrounds");
+});
+
+// USER PROFILE
+router.get("/users/:id",function(req,res){
+	User.findById(req.params.id,function(err,foundUser){
+		if(err){
+			req.flash("error","Something went wrong");
+			res.redirect("/");
+		}
+		Campground.find().where('author.id').equals(foundUser._id).exec(function(err,campgrounds){
+			if(err){
+				req.flash("error","Something went wrong");
+				res.redirect("/");
+			}
+			res.render("users/show",{user:foundUser,campgrounds:campgrounds});
+		});
+	});
 });
 
 function isLoggedIn(req,res,next){
